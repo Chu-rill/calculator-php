b7e148562aa91b2bd6b891847217d6f033fa1095
Chu-rill <churchillrimamskep@gmail.com>
2024-10-21 06:57:42 +0100

    done with the basic php calculator

--- index.php
+++ index.php
@@ -26,12 +26,15 @@
         exit();
         header("Location: ../index.php");
     }
+
+    //get user input
     $num01 = filter_input(INPUT_POST, 'num01', FILTER_SANITIZE_NUMBER_FLOAT);
     $num02 = filter_input(INPUT_POST, 'num02', FILTER_SANITIZE_NUMBER_FLOAT);
     $operator = htmlspecialchars($_POST['operator']);
 
     $errors = false;
 
+    //error handling
     if (empty($num01) || empty($num02) || empty($operator)) {
         echo "<p>fill in all fields</p>";
         $errors = true;
@@ -40,6 +43,29 @@
     if (!is_numeric($num01) || !is_numeric(($num02))) {
         echo "<p>only write numbers</p>";
         $errors = true;
+    }
+
+    //calculate the numbers
+    if (!$errors) {
+        $value = 0;
+        switch ($operator) {
+            case "add":
+                $value = $num01 + $num02;
+                break;
+            case "subtract";
+                $value = $num01 - $num02;
+                break;
+            case "multiply";
+                $value = $num01 * $num02;
+                break;
+            case "divide";
+                $value = $num01 / $num02;
+                break;
+            default:
+                echo "something went wrong";
+        }
+
+        echo "<h3>Result = " . $value . "</h3";
     }
     ?>
 </body>
